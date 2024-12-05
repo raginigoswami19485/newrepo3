23e7f37933fd59e588ecc85c020e9ea14a571b37
raginigoswami19485 <raginigoswami19485@gmail.com>
2024-12-05 19:34:42 +0530

    ports

--- data.js
+++ data.js
@@ -1,10 +1,62 @@
  function okk(){
+    
+   //  var a =5;
+   //  var b=8;
+   //  console .log(a)
+   // var a=4
+   // var b=28
+   // console.log(a+b) redeclear var allowed =a,a
+   
 
+   // let a=3
+   // let a=5   but let in redeclear not allowed
+
+//   const a=4
+//   const a=9    its is also not allowed redeclear
  
 
-
-    console.log("hello word")
- }
+//  q=> reassingment or allowed or not
 
 
- okk();
+// var a=5
+// a=9
+// console .log(a)  var in allowed for reassingment 
+
+
+// let a =0
+// a =9
+// console .log(a) its also allowed reassingment
+
+// const a=9
+// a=8
+// console.log(a) const is not allowed for reassingment
+
+// let a=5;
+// {
+//    let a=90
+//    console.log( "inner output="+(a))
+
+// }
+// console.log( "outer output="+(a))  let is block scoped
+
+// const a=35
+// {
+//    const a=99
+//    console.log(a) const is block scoped
+// }
+// console.log(a)
+  
+
+// var a=9
+// { var a= 45
+//    console.log(a)/  var is function scoped
+// }
+// console.log(a)
+//     console.log("hello word")
+
+
+
+}
+
+
+ okk(); 
