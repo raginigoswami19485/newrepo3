ba8a22fe410ce430afabe814e9916df647f1d003
raginigoswami19485 <raginigoswami19485@gmail.com>
2024-12-12 12:30:28 +0530

    yoyo

--- string.js
+++ string.js
@@ -1,22 +1,36 @@
-function yoyo(){
-
+function rag(){
+     
 // let text="hello ragini i m from bhopal";
-let text2="heloo" ;
-// alert(text.length)
+// alert(text.length) 
+// let text2="heloo";
 // document.write(text.length) to print in body
 // document.write(text.charAt(4)) pass the index number
 // document.write(text.slice(2,10))  llo rag
 // document.write(text.concat(" "+text2))
+//  let ans= document.write(text2.trim()
+// a.splice(2,0,39)
+// document.write(a)// map   
 
-         let ans= document.write(text2.trim())
+
+let a =[2,5,9,7,3,1,8,6]
+// let ans=a.map(function(num){
+//     return num*2
+// })
+// document.write(ans);
+// }
+}
+rag();
+
+
+let ans=a.map((num)=>num*2)
+ console.log(ans);
+
+const rag=()=>{
 
 
 
-         
 
 }
-yoyo();
-
 
 
 
