90328af0e90187adcbcd90e16b644425a17a3f42
raginigoswami19485 <raginigoswami19485@gmail.com>
2024-12-14 12:41:18 +0530

    js

--- string.js
+++ string.js
@@ -1,4 +1,4 @@
-function rag(){
+// function rag(){
      
 // let text="hello ragini i m from bhopal";
 // alert(text.length) 
@@ -10,28 +10,72 @@ function rag(){
 //  let ans= document.write(text2.trim()
 // a.splice(2,0,39)
 // document.write(a)// map   
-
-
-let a =[2,5,9,7,3,1,8,6]
 // let ans=a.map(function(num){
 //     return num*2
 // })
 // document.write(ans);
 // }
+function rag(){
+// let a=10;
+// if(a%2==0){
+//   alert("even")
+// }
+// else{
+//     alert("odd")
+// }
+// let a=22;
+// if(a>=18){
+//     alert("yes for vote")
+// }
+// else{
+//     alert("you are not ready for vote")
+// }
+
+// let age=;
+
+// let ans=a>=10?"adult":"child";
+// alert(ans)
+
+// alert("heeeloo")
+
+// let age=window.prompt("enter your age")
+//  let ans=age>=18? "yes for vote": "no";
+// alert(ans)
+
+// let marks=window.prompt("enter your marks")
+// let result=marks>=40? "pass":"fail";
+// alert(result)
+
+// let show=window.prompt("enter your no.")
+// if(33>=pass)
+//  alert("pass")
+// else{
+//   alert("pass")
+// }
+
+
+// let show1=window.prompt(" do you like wibside say yes")
+// let age=window.prompt("entre your age")
+// let result=age>=20?"adult":"child";
+// alert(result)
+
+
+
+// let age=window.prompt("enter your age")
+// if(age>=18){
+// console.log("adult")
+// }
+// else{
+//     console.log("child")
+// }
+ for(let i=12;i<=10;i++){
+    alert(i)
+ }
+
+
+
 }
 rag();
-
-
-let ans=a.map((num)=>num*2)
- console.log(ans);
-
-const rag=()=>{
-
-
-
-
-}
-
 
 
 
