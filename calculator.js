3600bdf13f95250e0007b6de87fc75a6b5a63e06
raginigoswami19485 <raginigoswami19485@gmail.com>
2025-01-03 18:33:18 +0530

    time

--- calculator.js
+++ calculator.js
@@ -1,27 +1,27 @@
-let store=""
-let cal=(value)=>{
+// let store=""
+// let cal=(value)=>{
      
-    let show=document.querySelector(".display")
-    store=store+value;
-    show.innerHTML=store;
+//     let show=document.querySelector(".display")
+//     store=store+value;
+//     show.innerHTML=store;
     
-}
-let result=()=>{
-    let show=document.querySelector(".display")
-    show.innerHTML=eval(store)
-    store=eval(store)
-    store.toString()
-}            
-let del=()=>{
-    let show=document.querySelector(".display")
-    store=store.slice(0 ,-1);
-   show.innerHTML=store
-}
-let ac=()=>{
-    let show=document.querySelector(".display")
-show.innerHTML=store
-store="";
-}
+// }
+// let result=()=>{
+//     let show=document.querySelector(".display")
+//     show.innerHTML=eval(store)
+//     store=eval(store)
+//     store.toString()
+// }            
+// let del=()=>{
+//     let show=document.querySelector(".display")
+//     store=store.slice(0 ,-1);
+//    show.innerHTML=store
+// }
+// let ac=()=>{
+//     let show=document.querySelector(".display")
+// show.innerHTML=store
+// store="";
+// }
 
 // for (let i = 1; i <= 5; i++) {
 //     console.log("apna college");
@@ -34,10 +34,30 @@ store="";
 // console.log(time.toLocaleString());
 // let time1=time.getFullYear()
 
-setInterval(()=>{
-    // let time=new Date();
-    let show=document.querySelector("#display")
-    let time=new Date()
-    let curntime=`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
-    show.innerHTML=curntime;
-},1000)
+// setInterval(()=>{
+//     // let time=new Date();
+//     let show=document.querySelector("#display")
+//     let time=new Date()
+//     let curntime=`${time.getHours()}:${time.getMinutes()}:${time.getSeconds ()}`
+//     show.innerHTML=curntime;
+// },1000)
+
+ let alert=()=>{
+ let inp=document.querySelector("#set").volue;
+ let show=document.querySelector("#display")
+  let inter=setInterval(()=>{
+    let time= new Date()
+    let alarmtime=`${time.getHours().toString().padStart(2,"0")}:${time.getMinutes().toString().padStart(2,"0")}`
+ if(alarmtime==inp){
+    show.innerHTML="alaram in runging"
+    document.querySelector("#aud").play()
+    setTimeout(()=>{
+        document.querySelector("#aud").pause()
+
+    },6000)
+}
+    else{
+        show.innerHTML="set alaram";
+     }
+ },1000)
+ }
